04b6792f26058dd466c83f6681270a4b3d821c14
Umut Sirin <umutsirin1@gmail.com>
2015-12-25 00:41:48 -0800

    Implement createInterface

---
+++ src/createInterface.js
@@ -0,0 +1,58 @@
+import map from 'lodash.map'
+import isInstanceOf from './isInstanceOf'
+import isInterface from './isInterface'
+
+export default function createInterface(interfaceName, interfaceDefinition) {
+  return {
+    /**
+     * This interface's name.
+     */
+    name: interfaceName,
+
+    /**
+     * Unique interface identifier. Mostly will be used for isInterface checks.
+     */
+    __interfaceSymbol: Symbol(interfaceName),
+
+    /**
+     * Checks if given candidate satisfies this interface's contract.
+     *
+     * @public
+     * @param {object} candidate - instance to be checked
+     * @param {function} extractValue
+     * @returns {boolean}
+     */
+    isInstance(candidate) {
+      return isInstanceOf(interfaceDefinition, candidate)
+    },
+
+    /**
+     * String representation of Interface.
+     *
+     * @public
+     * @returns {string}
+     */
+    toString() {
+      return `${interfaceName} ${definitionToString(interfaceDefinition)}`
+    }
+  }
+}
+
+/**
+ * Return string representation of given definition.
+ *
+ * @public
+ * @param {object} interfaceDefinition
+ * @returns {string}
+ */
+function definitionToString(interfaceDefinition) {
+  let stringified = map(interfaceDefinition, (value, key) => {
+    if (isInterface(value)) {
+      return `${key}: ${value.toString()}`
+    }
+    return `${key}: ${value.name}`
+  }).join(', ')
+
+  return `{ ${stringified} }`
+}
+
